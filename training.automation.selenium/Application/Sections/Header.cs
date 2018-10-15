d3f38cbec337be52fa1c43ad055002e4fdb80067
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-15 15:59:20 +0100

    Change add button to the header

--- training.automation.selenium/Application/Sections/Header.cs
+++ training.automation.selenium/Application/Sections/Header.cs
@@ -6,6 +6,7 @@ namespace training.automation.selenium.Application.Sections
 {
     public class Header : Section
     {
+        public Button addButton;
         public Image backToHome;
         public Image trelloLogoHome;
 
@@ -13,6 +14,7 @@ namespace training.automation.selenium.Application.Sections
 
         private void BuildSections()
         {
+            addButton = new Button(By.XPath("//span[@class=\"header-btn-icon icon-lg icon-add light\"]"), "Header + Button", name);
             backToHome = new Image(By.XPath("//*[@id=\"header\"]/div[1]/a/span"), "Back to Home button in Top Left", name);
             trelloLogoHome = new Image(By.XPath("//*[@id=\"header\"]/a/span[2]"), "Trello Logo Home Button", name);
         }
