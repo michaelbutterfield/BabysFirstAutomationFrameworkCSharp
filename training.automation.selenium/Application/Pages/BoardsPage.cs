83377b9c7b394d2a318c7549abba873b66033ed7
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-15 15:58:19 +0100

    Redo xpaths in correct way instead of ones copied from Chrome

--- training.automation.selenium/Application/Pages/BoardsPage.cs
+++ training.automation.selenium/Application/Pages/BoardsPage.cs
@@ -6,10 +6,8 @@ namespace training.automation.selenium.Application.Pages
 {
     public class BoardsPage : Page
     {
-        public Button addButton;
         public Button backgroundSelectionButton;
         public Label boardNotFound;
-        public Button boardsSideBarButton;
         public Button createBoardButton;
         public Button createNewBoardButton;
         public Button favouriteButton;
@@ -20,25 +18,13 @@ namespace training.automation.selenium.Application.Pages
 
         private void BuildPage()
         {
-            addButton = new Button(By.XPath("//*[@id=\"header\"]/div[5]/a[1]/span"), "Add Button", name);
-
-            backgroundSelectionButton = new Button(By.XPath("//*[@id=\"classic\"]/div[4]/div/div/div/form/div/ul/li[2]/button"),
-                                                   "Board Background Selection", name);
-
+            backgroundSelectionButton = new Button(By.XPath("//*[@id=\"classic\"]/div[4]/div/div/div/form/div/ul/li[2]/button"), "Board Background Selection", name);
             boardNotFound = new Label(By.XPath("//h1[contains(text(),'Board not found.')]"), "Board not found. message", name);
-
-            boardsSideBarButton = new Button(By.XPath("//*[@id=\"content\"]/div/div[2]/div/div/div/div/div/div[1]/nav/div[1]/ul/li[1]/a/span[2]"),
-                                             "Boards Side Bar Button", name);
-
-            createBoardButton = new Button(By.XPath("//*[@id=\"classic\"]/div[4]/div/div/div/form/button/span[2]"), "Create Board Button", name);
-
-            createNewBoardButton = new Button(By.XPath("//*[@id=\"classic\"]/div[5]/div/div[2]/div/div/div/ul/li[1]/a"), "Create Board... Button", name);
-
-            favouriteButton = new Button(By.XPath("//span[contains(@class, 'board-tile-options')]"), "Favourite Button", name);
-
-            nameInput = new InputBox(By.XPath("//*[@id=\"classic\"]/div[4]/div/div/div/form/div/div/div[1]/input"), "Board Name Input Box", name);
-
-            userBoardButton = new Button(By.XPath("//div[contains(@title, 'TestBoard')]"), "User Created Board", name);
+            createBoardButton = new Button(By.XPath("//button[@class=\"primary\"][@type=\"submit\"]"), "Create Board Button", name);
+            createNewBoardButton = new Button(By.XPath("//a[@class=\"js-new-board\"]"), "Create Board... Button", name);
+            favouriteButton = new Button(By.XPath("//span[@class='icon-sm icon-star board-tile-options-star-icon']"), "Favourite Button", name);
+            nameInput = new InputBox(By.XPath("//input[@placeholder='Add board title']"), "Board Name Input Box", name);
+            userBoardButton = new Button(By.XPath("//div[@title='TestBoard']"), "User Created Board", name);
         }
 
     }
