ac30a41560e65ae3bea0ff30069044efd216fc81
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-15 15:58:59 +0100

    Redo xpaths correctly instead of ones copied from Chrome

--- training.automation.selenium/Application/Pages/SpecificBoardsPage.cs
+++ training.automation.selenium/Application/Pages/SpecificBoardsPage.cs
@@ -30,24 +30,24 @@ namespace training.automation.selenium.Application.Pages
         private void BuildPage()
         {
             //Side Menu
-            closeBoard = new Button(By.XPath("//*[@id=\"content\"]/div/div[2]/div/div/div[2]/div/ul[3]/li/a"), "Close Board...", name);
-            closeBoardConfirmation = new Button(By.XPath("//*[@id=\"classic\"]/div[5]/div/div[2]/div/div/div/input"), "Confirm Close Board", name);
-            moreSideMenuButton = new Button(By.XPath("//*[@id=\"content\"]/div/div[2]/div/div/div[2]/div/ul/li[5]/a"), "More Button in Side Menu", name);
-            permDeleteBoard = new Link(By.XPath("//*[@id=\"content\"]/div/div/p[2]/a"), "Permanently Delete Board...", name);
-            permDeleteBoardConfirm = new Button(By.XPath("//*[@id=\"classic\"]/div[5]/div/div[2]/div/div/div/input"), "Yes, Permanently Delete Board.", name);
+            closeBoard = new Button(By.XPath("//a[@class='board-menu-navigation-item-link js-close-board']"), "Close Board...", name);
+            closeBoardConfirmation = new Button(By.XPath("//input[@class='js-confirm full negate']"), "Confirm Close Board", name);
+            moreSideMenuButton = new Button(By.XPath("//a[@class='board-menu-navigation-item-link js-open-more']"), "More Button in Side Menu", name);
+            permDeleteBoard = new Link(By.XPath("//a[@class='quiet js-delete']"), "Permanently Delete Board...", name);
+            permDeleteBoardConfirm = new Button(By.XPath("//input[@class='js-confirm full negate']"), "Yes, Permanently Delete Board.", name);
 
             //General
-            addACard = new Button(By.XPath("//span[contains(@class,'js-add-a-card')]"), "Add a card", name);
-            addAList = new Button(By.XPath("//a[contains(@class,'open-add-list js-open-add-list')]"), "Add a list", name);
-            addAnotherList = new Button(By.XPath("//div[contains(text(),'Add another list')]"), "Add another list (used for adding any list after the first one)", name);
-            addCardCancel = new Button(By.XPath("//a[contains(@class,'icon-lg icon-close dark-hover js-cancel')]"), "Add a card", name);
-            addCardButton = new Button(By.XPath("//input[contains(@class,'primary confirm mod-compact js-add-card')]"), "Add Card Button", name);
+            addACard = new Button(By.XPath("//span[@class='js-add-a-card']"), "Add a card", name);
+            addAList = new Button(By.XPath("//span[text()='Add a list']"), "Add a list", name);
+            addAnotherList = new Button(By.XPath("//span[text()='Add another list']"), "Add another list (used for adding any list after the first one)", name);
+            addCardCancel = new Button(By.XPath("//a[@class='icon-lg icon-close dark-hover js-cancel']"), "Add a card cancel", name);
+            addCardButton = new Button(By.XPath("//input[@class='primary confirm mod-compact js-add-card'][@type='submit']"), "Add Card Button", name);
             addCardFirstCol = new Button(By.XPath("//*[@id=\"board\"]/div[1]/div/a/span[2]"), "Add a card (First column)", name);
             addCardSecondCol = new Button(By.XPath("//*[@id=\"board\"]/div[2]/div/a/span[2]"), "Add a card (Second column)", name);
             addCardThirdCol = new Button(By.XPath("//*[@id=\"board\"]/div[3]/div/a/span[2]"), "Add a card (Second column)", name);
-            addListButton = new Button(By.XPath("//input[contains(@class,'primary mod-list-add-button js-save-edit')]"), "Add List Confirmation Button", name);
-            enterCardTitle = new InputBox(By.XPath("//textarea[contains(@class, 'list-card-composer-textarea js-card-title')]"), "Entering card title", name);
-            enterListTitle = new InputBox(By.XPath("//input[contains(@class,'list-name-input')]"), "Entering the list title", name);
+            addListButton = new Button(By.XPath("//input[@class='primary mod-list-add-button js-save-edit']"), "Add List Confirmation Button", name);
+            enterCardTitle = new InputBox(By.XPath("//textarea[@class='list-card-composer-textarea js-card-title']"), "Entering card title", name);
+            enterListTitle = new InputBox(By.XPath("//input[@class='list-name-input']"), "Entering the list title", name);
         }
     }
 }
