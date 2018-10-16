17763338a1ce3ba3e03b02adf11e301581b79924
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-16 12:06:51 +0100

    For the model that appears when creating a new board

---
+++ training.automation.selenium/Application/Pages/CreateBoardPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using training.automation.common.Pages;
+using training.automation.common.Selenium.Elements;
+
+namespace training.automation.selenium.Application.Pages
+{
+    public class CreateBoardPage : Page
+    {
+        public Button backgroundSelectionButton;
+        public Button createBoardButton;
+        public InputBox nameInput;
+
+        public CreateBoardPage() : base("Create Board Page") { BuildPage(); }
+
+        private void BuildPage()
+        {
+            backgroundSelectionButton = new Button(By.XPath("//*[@id=\"classic\"]/div[4]/div/div/div/form/div/ul/li[2]/button"), "Board Background Selection", name);
+            createBoardButton = new Button(By.XPath("//button[@class=\"primary\"][@type=\"submit\"]"), "Create Board Button", name);
+            nameInput = new InputBox(By.XPath("//input[@placeholder='Add board title']"), "Board Name Input Box", name);
+        }
+    }
+}
