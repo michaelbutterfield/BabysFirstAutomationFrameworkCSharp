89f64798b1e16dc7d7258c45c3a460207126ca3c
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-16 12:08:48 +0100

    Replace HomePage

---
+++ training.automation.selenium/Application/Pages/SplashPage.cs
@@ -0,0 +1,20 @@
+﻿using training.automation.common.Pages;
+using training.automation.common.Selenium.Elements;
+using OpenQA.Selenium;
+
+namespace training.automation.selenium.Application.Pages
+{
+    public class SplashPage : Page
+    {
+        public Button logIn;
+        public Button signUp;
+
+        public SplashPage() : base("Splash Page") { BuildPage(); }
+
+        private void BuildPage()
+        {
+            logIn = new Button(By.XPath("//a[@href='/login?returnUrl=%2F']"), "Log In Button", name);
+            signUp = new Button(By.XPath("//a[@href='/signup?returnUrl=%2F']"), "Sign Up Button", name);
+        }
+    }
+}
