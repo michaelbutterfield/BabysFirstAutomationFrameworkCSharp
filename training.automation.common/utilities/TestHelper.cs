d1e3ff3b622074a58db65bd1b7076a6f4169c1fb
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-18 14:01:39 +0100

    Implement TestContext for the TestLogger

--- training.automation.common/utilities/TestHelper.cs
+++ training.automation.common/utilities/TestHelper.cs
@@ -3,11 +3,14 @@ using System.Text;
 using System.Threading;
 using NHamcrest;
 using NHamcrest.Core;
+using NUnit.Framework;
 
 namespace training.automation.common.utilities
 {
     public class TestHelper
     {
+        private static TestContext scenario;
+
         public static void AssertThat<T>(T actual, IMatcher<T> matcher, String stepDescription)
         {
             AssertThat(actual, matcher, stepDescription, true);
@@ -34,6 +37,13 @@ namespace training.automation.common.utilities
             }
         }
 
+        public static TestContext GetScenario()
+        {
+            scenario = TestContext.CurrentContext;
+
+            return scenario;
+        }
+
         public static void HandleException(String errorMessage, Exception e, Boolean takeScreenshot)
         {
             String exception = String.Format("{0} : {1} : ", errorMessage, e);
@@ -43,6 +53,21 @@ namespace training.automation.common.utilities
             TestHelper.WriteToConsole(exception);
 
             throw new System.ArgumentException(e.Message, e);
+        }
+
+        public static String GetTodaysDateTime(String format)
+        {
+            return DateTime.Now.ToString(format);
+        }
+
+        public static bool ScenarioHasTag(String tagName)
+        {
+            return GetScenario().Test.Name.Contains(tagName);
+        }
+
+        public static void SetScenario(TestContext givenScenario)
+        {
+            scenario = givenScenario;
         }
 
         public static void SleepInSeconds(int seconds)
