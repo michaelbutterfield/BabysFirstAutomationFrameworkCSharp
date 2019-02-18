129f03dcd771ff8030595d3fa2ee0c36574f6afb
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-02-18 15:06:30 +0000

    Move web data utility to selenium specifically

---
+++ training.automation.selenium.specflow/Application/Data/TrelloWebData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using training.automation.common.utilities;
+
+namespace training.automation.specflow.Data
+{
+    class TrelloWebData
+    {
+        private static String username;
+        private static String password;
+
+        public TrelloWebData() { }
+
+        public static String GetUsername()
+        {
+            return username;
+        }
+
+        public static String GetPassword()
+        {
+            return password;
+        }
+
+        public static void ReadUserPass()
+        {
+            try
+            {
+                string sourceFile = "C:\\Users\\michael.butterfield\\Desktop\\trellouserpass.txt";
+
+                string line = System.IO.File.ReadAllText(@sourceFile);
+
+                String[] lines = line.Split('\t');
+
+                username = lines[0];
+
+                password = lines[1];
+            }
+            catch (Exception e)
+            {
+                String errorMessage = String.Format("Could not read username and password from file");
+
+                TestHelper.HandleException(errorMessage, e, false);
+            }
+        }
+    }
+}
