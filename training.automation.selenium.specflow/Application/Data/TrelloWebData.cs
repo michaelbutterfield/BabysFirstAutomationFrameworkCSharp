d0950ce507262801651aae27157d40ed99bf9218
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-02-19 13:41:09 +0000

    Revert "Fix sourceFile for local repo"
    This reverts commit db589ec7987685aaf3b7457774fd9700ccd6b601.

--- training.automation.selenium.specflow/Application/Data/TrelloWebData.cs
+++ training.automation.selenium.specflow/Application/Data/TrelloWebData.cs
@@ -24,8 +24,7 @@ namespace training.automation.specflow.Data
         {
             try
             {
-                //string sourceFile = "C:\\Users\\michael.butterfield\\Desktop\\trellouserpass.txt";
-                string sourceFile = "C:\\Users\\M\\Desktop\\trellouserpass.txt";
+                string sourceFile = "C:\\Users\\michael.butterfield\\Desktop\\trellouserpass.txt";
 
                 string line = System.IO.File.ReadAllText(@sourceFile);
 
