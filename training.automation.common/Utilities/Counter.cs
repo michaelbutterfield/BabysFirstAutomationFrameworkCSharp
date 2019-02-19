bdc4bce7d5d4f8e0a68fb87186749125e40019e0
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-02-19 13:52:43 +0000

    Revert "Revert "Backend framework stuff""
    This reverts commit f8a95b1db3db8760205d3baf43c76511bb157fb3.

---
+++ training.automation.common/Utilities/Counter.cs
@@ -0,0 +1,8 @@
+﻿namespace training.automation.specflow.Application.Data
+{
+    public class Counter
+    {
+        public static int CardsCreated = 0;
+        public static int CardsMoved = 0;
+    }
+}
