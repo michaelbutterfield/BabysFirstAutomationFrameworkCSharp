97c3814d4502ba2ef9bb44922cb677289e9deb0b
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-15 13:52:03 +0000

    Add RuntimeTestData class to store objects through step defs etc

---
+++ training.automation.common/Utilities/RuntimeTestData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace training.automation.common.Utilities
+{
+    public class RuntimeTestData
+    {
+        private static Dictionary<string, object> TestData;
+
+        static RuntimeTestData()
+        {
+            TestData = new Dictionary<string, object>();
+        }
+
+        public static void Add(string Key, string Value)
+        {
+            TestData.Add(Key, Value);
+        }
+
+        public static string GetAsString(string key)
+        {
+            return TestData[key].ToString();
+        }
+
+        public static void Destroy()
+        {
+            TestData.Clear();
+        }
+    }
+}
