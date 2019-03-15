2f1f3628bbf129bd366789bbe7fa1060aa57d5c4
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-15 16:00:59 +0000

    Move log in page steps to log in page - more compliant to page object model

--- training.automation.selenium.specflow/Test/CSharp/StepDefinitions/SplashPageSteps.cs
+++ training.automation.selenium.specflow/Test/CSharp/StepDefinitions/SplashPageSteps.cs
@@ -11,19 +11,8 @@ namespace training.automation.specflow.Test.CSharp.StepDefinitions
         [Given(@"I am on the splash page")]
         public void IAmOnTheSplashPage()
         {
-            SeleniumHelper.GetWebDriver().Navigate().GoToUrl("http://trello.com/");
             DesktopWebsite.SplashPage.LogIn.WaitUntilExists();
             DesktopWebsite.SplashPage.LogIn.AssertElementIsDisplayed();
-        }
-
-        [Given(@"I log in")]
-        public void ILogIn()
-        {
-            DesktopWebsite.SplashPage.LogIn.Click();
-            DesktopWebsite.LogInPage.ForgotPassword.WaitForElementToBeClickable();
-            DesktopWebsite.LogInPage.EmailAddress.InputText(TrelloWebData.GetUsername());
-            DesktopWebsite.LogInPage.Password.InputText(TrelloWebData.GetPassword());
-            DesktopWebsite.LogInPage.LogIn.Click();
         }
     }
 }
