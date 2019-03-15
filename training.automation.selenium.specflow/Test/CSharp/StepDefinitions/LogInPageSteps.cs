36cd4c47c671f2c35559f9a5248c65abcef3baf7
M <M@DESKY>
2019-03-15 22:13:35 +0000

    Changes to assertions (addition of AssertExists()) Change to element attribute retrieved

--- training.automation.selenium.specflow/Test/CSharp/StepDefinitions/LogInPageSteps.cs
+++ training.automation.selenium.specflow/Test/CSharp/StepDefinitions/LogInPageSteps.cs
@@ -9,7 +9,7 @@ namespace training.automation.selenium.specflow.Test.CSharp.StepDefinitions
     [Binding]
     public sealed class LogInPageSteps
     {
-        [Given][When][Then]
+        [Given]
         public void I_Log_In()
         {
             DesktopWebsite.SplashPage.LogIn.Click();
@@ -20,17 +20,31 @@ namespace training.automation.selenium.specflow.Test.CSharp.StepDefinitions
         }
 
         [Given]
+        public void I_am_on_the_log_in_page()
+        {
+            DesktopWebsite.LogInPage.EmailAddress.WaitUntilExists();
+            DesktopWebsite.LogInPage.EmailAddress.AssertElementIsDisplayed();
+        }
+
+        [Given][When]
         public void I_enter_the_user_details()
         {
             DesktopWebsite.LogInPage.EmailAddress.SendKeys(TrelloWebData.GetUsername());
             DesktopWebsite.LogInPage.Password.SendKeys(TrelloWebData.GetPassword());
         }
 
+        [When]
+        public void I_click_log_in()
+        {
+            DesktopWebsite.LogInPage.LogIn.Click();
+        }
+
+
         [Then]
         public void I_will_be_logged_in_successfully()
         {
-            DesktopWebsite.BoardsPage.CreateNewBoard.WaitUntilExists();
-            DesktopWebsite.BoardsPage.CreateNewBoard.AssertExists();
+            DesktopWebsite.Header.BackToHome.WaitUntilExists();
+            DesktopWebsite.Header.BackToHome.AssertExists();
         }
 
         [Given][When]
@@ -50,7 +64,7 @@ namespace training.automation.selenium.specflow.Test.CSharp.StepDefinitions
         [Then]
         public void the_error_P0_will_be_shown(string p0)
         {
-            string actual = DesktopWebsite.LogInPage.ErrorMessage.GetElementAttribute("text()");
+            string actual = DesktopWebsite.LogInPage.ErrorMessage.GetElementAttribute("innerText");
             TestHelper.AssertThat(actual, Is.EqualTo(p0), string.Format("Assert the error {0} is equal to {1}", p0, actual), false);
         }
     }
