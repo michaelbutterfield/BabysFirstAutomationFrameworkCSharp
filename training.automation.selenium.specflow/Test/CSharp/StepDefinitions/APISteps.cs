347e27b18afee8ae869215b413ee1cf9ce4a8910
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-15 13:54:30 +0000

    Start of API implementation

---
+++ training.automation.selenium.specflow/Test/CSharp/StepDefinitions/APISteps.cs
@@ -0,0 +1,15 @@
+﻿using TechTalk.SpecFlow;
+using training.automation.common.Utilities;
+
+namespace training.automation.selenium.specflow.Test.CSharp.StepDefinitions
+{
+    class APISteps
+    {
+        [Given]
+        public void I_create_a_board_through_the_API()
+        {
+            RuntimeTestData.Add("BoardName", Random.RandomString(10));
+        }
+
+    }
+}
