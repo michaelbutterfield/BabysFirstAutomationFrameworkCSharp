204031cc9194effaf40f3bb4cc32b1424de95d27
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-15 13:51:23 +0000

    Rename RandomGen to Random - easier to remember/use

---
+++ training.automation.common/Utilities/Random.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace training.automation.common.Utilities
+{
+    public class Random
+    {
+        private static System.Random random = new System.Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
