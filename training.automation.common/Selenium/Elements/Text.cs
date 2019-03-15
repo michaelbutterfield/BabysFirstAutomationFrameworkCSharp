b82273fa474929426ff721d5c7f628ac098ac57d
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-15 15:58:00 +0000

    New text element

---
+++ training.automation.common/Selenium/Elements/Text.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+using training.automation.common.selenium.elements.common;
+
+namespace training.automation.common.Selenium.Elements
+{
+    public class Text : Element
+    {
+        public Text(By myLocator, string elementName, string pageName) : base(myLocator, elementName, pageName) { }
+    }
+}
