3756a4fcf1f87adb673b9313a1cdc16f82663137
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-26 13:18:38 +0000

    Add a Get for generic objects, not just strings

--- training.automation.common/Utilities/RuntimeTestData.cs
+++ training.automation.common/Utilities/RuntimeTestData.cs
@@ -11,14 +11,19 @@ namespace training.automation.common.Utilities
             TestData = new Dictionary<string, object>();
         }
 
-        public static void Add(string Key, string Value)
+        public static void Add(string Key, object Value)
         {
             TestData.Add(Key, Value);
         }
 
-        public static string GetAsString(string key)
+        public static object Get(string Key)
         {
-            return TestData[key].ToString();
+            return TestData[Key];
+        }
+
+        public static string GetAsString(string Key)
+        {
+            return TestData[Key].ToString();
         }
 
         public static void Destroy()
