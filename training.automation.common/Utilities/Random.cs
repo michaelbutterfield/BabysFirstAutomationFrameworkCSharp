0437026364c9cc88beb8f1ad5ddb0fd56cae3d97
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-27 08:26:51 +0000

    Addition of Random alphabet string and random sentence

--- training.automation.common/Utilities/Random.cs
+++ training.automation.common/Utilities/Random.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Text;
 
 namespace training.automation.common.Utilities
 {
@@ -11,6 +12,39 @@ namespace training.automation.common.Utilities
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string RandomAlphabetString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+        public static string RandomSentence(int WordCount)
+        {
+            string[] words = { "Lorem", "ipsum", "dolor", "sit", "amet", "cursus", "posuere", "vehicula", "integer", "tincidunt",
+                               "nam", "laoreet", "ac", "at", "imperdiet", "vitae", "pede", "ut", "tempus", "sit", "sed", "integer",
+                               "egestas", "iaculis", "eget", "eu" };
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < WordCount; i++)
+            {
+                // Select a random word from the array
+                builder.Append(words[random.Next(words.Length)]).Append(" ");
+            }
+
+            string sentence = builder.ToString().Trim() + ". ";
+
+            // Set the first letter of the first word in the sentenece to uppercase
+            sentence = char.ToUpper(sentence[0]) + sentence.Substring(1);
+
+            builder = new StringBuilder();
+            builder.Append(sentence);
+
+            return builder.ToString();
         }
     }
 }
