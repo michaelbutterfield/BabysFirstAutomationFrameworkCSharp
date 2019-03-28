5bc3572d214e3dcb442effd8ed173dc162caef32
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-28 14:11:43 +0000

    Addition of various pages

--- training.automation.selenium.specflow/Application/DesktopWebsite.cs
+++ training.automation.selenium.specflow/Application/DesktopWebsite.cs
@@ -1,11 +1,15 @@
-﻿using training.automation.specflow.Application.Pages;
+﻿using training.automation.selenium.specflow.Application.Pages;
+using training.automation.selenium.specflow.Application.Pop_ups;
+using training.automation.specflow.Application.Pages;
 using training.automation.specflow.Application.Sections;
 
 namespace training.automation.specflow.Application
 {
     class DesktopWebsite
     {
+        public static AddChecklist AddChecklist;
         public static BoardsPage BoardsPage;
+        public static CardPage CardPage;
         public static CreateBoardPage CreateBoardPage;
         public static Header Header;
         public static LogInPage LogInPage;
@@ -16,11 +20,13 @@ namespace training.automation.specflow.Application
         {
             BuildPages();
             BuildSections();
+            BuildPopUps();
         }
 
         private static void BuildPages()
         {
             BoardsPage = new BoardsPage();
+            CardPage = new CardPage();
             CreateBoardPage = new CreateBoardPage();
             LogInPage = new LogInPage();
             SpecificBoardsPage = new SpecificBoardsPage();
@@ -30,6 +36,11 @@ namespace training.automation.specflow.Application
         private static void BuildSections()
         {
             Header = new Header();
+        }
+
+        private static void BuildPopUps()
+        {
+            AddChecklist = new AddChecklist();
         }
     }
 }
