065cfc83ad99d355397bdf4ef8349640c2ed1b88
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-28 13:59:49 +0000

    Create Pop-up class for card checklists etc.

---
+++ training.automation.common/Pop-up/Pop-up.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace training.automation.common.Pop_up
+{
+    public abstract class Pop_up
+    {
+        protected String name;
+
+        protected Pop_up(String name)
+        {
+            this.name = name;
+        }
+    }
+}
