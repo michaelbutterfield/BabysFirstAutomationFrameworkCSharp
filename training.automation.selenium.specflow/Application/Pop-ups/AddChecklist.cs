6aa5e46d56dd45a86c0b848d73b192a25b529611
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-28 14:11:03 +0000

    AddChecklist pop up for card page

---
+++ training.automation.selenium.specflow/Application/Pop-ups/AddChecklist.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using training.automation.common.Selenium.Elements;
+
+namespace training.automation.selenium.specflow.Application.Pop_ups
+{
+    public class AddChecklist : common.Pop_up.Pop_up
+    {
+        public AddChecklist() : base("Add Checklist") { BuildPage(); }
+
+        public InputBox ChecklistTitle;
+        public Button Add;
+
+        private void BuildPage()
+        {
+            ChecklistTitle = new InputBox(By.XPath("//input[@id='id-checklist' and @type='text']"), "Checklist Title", name);
+            Add = new Button(By.XPath("//input[@value='Add' and @type='submit']"), "Add", name);
+        }
+    }
+}
