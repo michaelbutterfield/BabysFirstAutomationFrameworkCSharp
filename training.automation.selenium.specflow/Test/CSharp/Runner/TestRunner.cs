33f603c8f077e3fbb53a9529f8ca339679805435
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-03-29 16:32:27 +0000

    AfterTestRun - fix initialiser

--- training.automation.selenium.specflow/Test/CSharp/Runner/TestRunner.cs
+++ training.automation.selenium.specflow/Test/CSharp/Runner/TestRunner.cs
@@ -1,5 +1,4 @@
-﻿using RestSharp;
-using TechTalk.SpecFlow;
+﻿using TechTalk.SpecFlow;
 using training.automation.api.Data;
 using training.automation.api.Utilities;
 using training.automation.common.Tests;
@@ -7,7 +6,6 @@ using training.automation.common.utilities;
 using training.automation.common.Utilities;
 using training.automation.specflow.Application;
 using training.automation.specflow.Data;
-using training.automation.specflow.Test.CSharp.StepDefinitions;
 
 namespace training.automation.specflow.Test.CSharp.Runner
 {
@@ -17,20 +15,21 @@ namespace training.automation.specflow.Test.CSharp.Runner
         [BeforeTestRun]
         static void BeforeTestRun()
         {
-            TestLogger.Initialise();
-            TestLogger.LogSuiteSetupStart();
             TrelloWebData.ReadUserPass();
             TrelloApiData.ReadApiKeyToken();
         }
 
+        [AfterTestRun]
         static void AfterTestRun()
         {
-            TestLogger.Close();
+
         }
 
         [BeforeScenario]
         static void BeforeScenario()
         {
+            TestLogger.Initialise();
+            TestLogger.LogScenarioStart();
             SeleniumHelper.Initialise("chrome");
         }
 
@@ -50,6 +49,8 @@ namespace training.automation.specflow.Test.CSharp.Runner
 
             SeleniumHelper.DestroyDriver();
             RuntimeTestData.Destroy();
+            TestLogger.LogScenarioEnd();
+            TestLogger.Close();
         }
     }
 }
