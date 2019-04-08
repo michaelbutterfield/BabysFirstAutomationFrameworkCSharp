5bba57ffaacf2980a239d41cfd8ba2dcc45d3cdc
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-04-08 08:05:09 +0100

    Start of appium helper class

---
+++ training.automation.common/Utilities/AppiumHelper.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace training.automation.common.Utilities
+{
+    //public class AppiumHelper
+    //{
+    //    //private AppiumDriver<IWebElement> driver;
+    //    //private string bundleId;
+
+    //    private static Uri testServerAddress = new Uri("http://127.0.0.1:4723/wd/hub");
+
+    //    public void InitialiseAndroid()
+    //    {
+
+    //    }
+
+    //    public void InitialiseIOS(string application)
+    //    {
+    //        switch(application.ToLower())
+    //        {
+    //            case "contacts":
+    //                {
+    //                    //bundleId = "com.apple.MobileAddressBook";
+    //                    break;
+    //                }
+    //        }
+
+    //        //DesiredCapabilities capabilities = new DesiredCapabilities();
+
+    //        //OpenQA.Selenium.Appium.Service.Options options = new OpenQA.Selenium.Appium.Service.Options(); 
+
+    //        //capabilities.SetCapability("automationName", "XCUITest");
+    //        //capabilities.SetCapability("platformName", "iOS");
+    //        //capabilities.SetCapability("updatedWDABundleId", "com.roq.WebDriverAgentRunner");
+    //        //capabilities.SetCapability("udid", "0464173d5acf9c1c5375ddf6d447b72c111d6ff4");
+    //        //capabilities.SetCapability("deviceName", "RoqIT Ipad");
+    //        //capabilities.SetCapability("platformVersion", "10.2.1");
+    //        //capabilities.SetCapability("bundleId", bundleId);
+    //    }
+
+    //}
+}
