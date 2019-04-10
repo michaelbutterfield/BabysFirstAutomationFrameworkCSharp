a44e2a2f66a3074afc77eb73b9c33ff60add83cf
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-04-10 11:17:15 +0100

    Completely blitz and fix appium helper

--- training.automation.common/Utilities/AppiumHelper.cs
+++ training.automation.common/Utilities/AppiumHelper.cs
@@ -1,49 +1,69 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.iOS;
 using OpenQA.Selenium.Remote;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace training.automation.common.Utilities
 {
-    //public class AppiumHelper
-    //{
-    //    //private AppiumDriver<IWebElement> driver;
-    //    //private string bundleId;
+    public class AppiumHelper
+    {
+        public static TimeSpan DEFAULT_TIMEOUT = new TimeSpan(0, 0, 10);
+        private static AppiumDriver<AppiumWebElement> driver;
+        private static Uri testServerAddress = new Uri("http://127.0.0.1:4723/wd/hub");
 
-    //    private static Uri testServerAddress = new Uri("http://127.0.0.1:4723/wd/hub");
+        public void InitialiseAndroid()
+        {
 
-    //    public void InitialiseAndroid()
-    //    {
+        }
 
-    //    }
+        public void InitialiseAndroid(string applicationId)
+        {
 
-    //    public void InitialiseIOS(string application)
-    //    {
-    //        switch(application.ToLower())
-    //        {
-    //            case "contacts":
-    //                {
-    //                    //bundleId = "com.apple.MobileAddressBook";
-    //                    break;
-    //                }
-    //        }
+        }
 
-    //        //DesiredCapabilities capabilities = new DesiredCapabilities();
+        public void InitialiseIOS(string application)
+        {
+            string BundleId = null;
 
-    //        //OpenQA.Selenium.Appium.Service.Options options = new OpenQA.Selenium.Appium.Service.Options(); 
+#pragma warning disable CS0618 // Type or member is obsolete
+            DesiredCapabilities capabilities = new DesiredCapabilities();
 
-    //        //capabilities.SetCapability("automationName", "XCUITest");
-    //        //capabilities.SetCapability("platformName", "iOS");
-    //        //capabilities.SetCapability("updatedWDABundleId", "com.roq.WebDriverAgentRunner");
-    //        //capabilities.SetCapability("udid", "0464173d5acf9c1c5375ddf6d447b72c111d6ff4");
-    //        //capabilities.SetCapability("deviceName", "RoqIT Ipad");
-    //        //capabilities.SetCapability("platformVersion", "10.2.1");
-    //        //capabilities.SetCapability("bundleId", bundleId);
-    //    }
 
-    //}
+            switch (application.ToLower())
+            {
+                case "contacts":
+                    {
+                        BundleId = "com.apple.MobileAddressBook";
+                        break;
+                    }
+            }
+
+            capabilities.SetCapability("automationName", "XCUITest");
+            capabilities.SetCapability("platformName", "iOS");
+            capabilities.SetCapability("updatedWDABundleId", "com.roq.WebDriverAgentRunner");
+            capabilities.SetCapability("udid", "0464173d5acf9c1c5375ddf6d447b72c111d6ff4");
+            capabilities.SetCapability("deviceName", "RoqIT Ipad");
+            capabilities.SetCapability("platformVersion", "10.2.1");
+            capabilities.SetCapability("bundleId", BundleId);
+#pragma warning restore CS0618 // Type or member is obsolete
+            driver = new IOSDriver<AppiumWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities, TimeSpan.FromSeconds(180));
+        }
+
+        public static AppiumDriver<AppiumWebElement> GetDriver()
+        {
+            return driver;
+        }
+
+        public static AppiumWebElement FindElement(By locator)
+        {
+            return driver.FindElement(locator);
+        }
+
+        public static ReadOnlyCollection<AppiumWebElement> FindElements(By locator)
+        {
+            return driver.FindElements(locator);
+        }
+    }
 }
