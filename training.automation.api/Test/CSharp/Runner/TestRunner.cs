eaa3a1d26413d1bdbc59eb3b993ad2732d07cd03
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2019-05-23 10:07:07 +0100

    Addition of Specflow to API tests

---
+++ training.automation.api/Test/CSharp/Runner/TestRunner.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using TechTalk.SpecFlow;
+using training.automation.api.Data;
+using training.automation.common.Tests;
+using training.automation.common.Utilities;
+
+namespace training.automation.api.Test.CSharp.Runner
+{
+    [Binding]
+    public class TestRunner
+    {
+        [BeforeTestRun]
+        static void BeforeTestRun()
+        {
+            TrelloApiData.ReadApiKeyToken();
+        }
+
+        [AfterTestRun]
+        static void AfterTestRun()
+        {
+
+        }
+
+        [BeforeScenario]
+        static void BeforeScenario()
+        {
+            TestLogger.Initialise();
+            TestLogger.LogScenarioStart();
+        }
+
+        [AfterScenario]
+        static void AfterScenario()
+        {
+            TestContext scenario = TestHelper.GetScenario();
+
+            if (scenario.Result.Outcome.Status.Equals(TestStatus.Failed))
+            {
+                FailureScreenshot.TakeScreenshot();
+            }
+
+            RuntimeTestData.Destroy();
+            TestLogger.LogScenarioEnd();
+            TestLogger.Close();
+        }
+    }
+}
---
+++ training.automation.api/Test/CSharp/StepDefinitions/TrelloSearchSteps.cs
@@ -0,0 +1,54 @@
+﻿using RestSharp;
+using TechTalk.SpecFlow;
+using training.automation.api.Data;
+
+namespace training.automation.api.Test.CSharp.StepDefinitions
+{
+    [Binding]
+    public sealed class TrelloSearchSteps
+    {
+        [Given]
+        public void I_retrieve_the_API_credentials()
+        {
+            TrelloApiData.ReadApiKeyToken();
+        }
+
+        [When]
+        public string I_send_a_search_request()
+        {
+            var client = new RestClient("http://api.trello.com");
+
+            var request = new RestRequest("/1/search?query={query}&key={key}&token={token}", Method.GET);
+            request.AddUrlSegment("query", "permboard");
+            request.AddUrlSegment("key", TrelloApiData.GetApiKey());
+            request.AddUrlSegment("token", TrelloApiData.GetApiToken());
+
+            var response = client.Execute<TrelloQueryResponseData.RootObject>(request);
+
+            return response.StatusCode.ToString();
+        }
+
+        [When]
+        public string I_send_a_bad_search_request()
+        {
+            var client = new RestClient("http://api.trello.com");
+
+            var request = new RestRequest("/1/search?query={query}&key={key}&token={token}", Method.GET);
+            request.AddUrlSegment("query", "boardnotthere");
+            request.AddUrlSegment("key", TrelloApiData.GetApiKey());
+            request.AddUrlSegment("token", TrelloApiData.GetApiToken());
+
+            var response = client.Execute<TrelloQueryResponseData.RootObject>(request);
+
+            return response.StatusCode.ToString();
+        }
+
+
+        [Then]
+        public void I_will_recieve_a_P0_response(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
